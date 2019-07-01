651c3918d045cb4a40978b1ee4d68e3cd282f4fd
Gabor Gerencser <kispeka94@gmail.com>
2019-07-01 22:39:12 +0200

    Fxed asc-desc

--- script.js
+++ script.js
@@ -14,10 +14,8 @@ $(() => {
     $('#loading').addClass('d-none');
   }
 
-
-
   // rendezés főváros szerint
-  function sortByCapital() {
+  function sortByCapitalDown() {
     dataList = dataList.sort(function(a, b) {
       var x = a.capital.toLowerCase();
       var y = b.capital.toLowerCase();
@@ -25,8 +23,16 @@ $(() => {
     });
   };
 
+  function sortByCapitalUp() {
+    dataList = dataList.sort(function(a, b) {
+      var x = a.capital.toLowerCase();
+      var y = b.capital.toLowerCase();
+      return x > y ? -1 : x > y ? 1 : 0;
+    });
+  };
+
   // rendezés név szerint
-  function sortByName() {
+  function sortByNameDown() {
     dataList = dataList.sort(function(a, b) {
       var x = a.name.toLowerCase();
       var y = b.name.toLowerCase();
@@ -34,17 +40,37 @@ $(() => {
     });
   };
 
+  function sortByNameUp() {
+    dataList = dataList.sort(function(a, b) {
+      var x = a.name.toLowerCase();
+      var y = b.name.toLowerCase();
+      return x > y ? -1 : x > y ? 1 : 0;
+    });
+  };
+
   // rendezés szám szerint
-  function sortByPopulation() {
+  function sortByPopulationDown() {
     dataList = dataList.sort(function(a, b) {
       return a.population-b.population;
     });
   };
 
+  function sortByPopulationUp() {
+    dataList = dataList.sort(function(a, b) {
+      return b.population-a.population;
+    });
+  };
+
   // rendezés terület szerint
-  function sortByArea() {
+  function sortByAreaDown() {
     dataList = dataList.sort(function(a, b) {
       return a.area-b.area;
+    });
+  };
+
+  function sortByAreaUp() {
+    dataList = dataList.sort(function(a, b) {
+      return b.area-a.area;
     });
   };
 
@@ -97,46 +123,75 @@ $(() => {
     method: 'GET',
   });
 
-  // rendezés ország szerint
-  $('table tr i.country').click(function(){
-    deleteTr;
-    sortByName();
-    refreshTable(dataList);
-  });
-
     // rendezés főváros szerint
   $('table tr i.capital').click(function(){
     deleteTr;
-    sortByCapital();
+    if ($('table tr i.capital').hasClass('fa-sort-alpha-down')) {
+      $('table tr i.capital').removeClass('fas fa-sort-alpha-down');
+      $('table tr i.capital').addClass('fas fa-sort-alpha-up-alt');
+      sortByCapitalDown();
+    }else{
+      sortByCapitalUp();
+      $('table tr i.capital').removeClass('fas fa-sort-alpha-up-alt');
+      $('table tr i.capital').addClass('fas fa-sort-alpha-down');
+    }
     refreshTable(dataList);
   });
 
-    // rendezés terület szerint
+    // rendezés ország szerint
+  $('table tr i.country').click(function(){
+    deleteTr;
+    if ($('table tr i.country').hasClass('fa-sort-alpha-down')) {
+      $('table tr i.country').removeClass('fas fa-sort-alpha-down');
+      $('table tr i.country').addClass('fas fa-sort-alpha-up-alt');
+      sortByNameDown();
+    }else{
+      $('table tr i.country').removeClass('fas fa-sort-alpha-up-alt');
+      $('table tr i.country').addClass('fas fa-sort-alpha-down');
+      sortByNameUp();
+    }
+    refreshTable(dataList);
+  });
+
+  // rendezés terület szerint
   $('table tr i.area').click(function(){
     deleteTr;
-    sortByArea();
+    if ($('table tr i.area').hasClass('fa-sort-numeric-down')) {
+      $('table tr i.area').removeClass('fa-sort-numeric-down');
+      $('table tr i.area').addClass('fa-sort-numeric-up-alt');
+      sortByAreaDown();
+    }else{
+      $('table tr i.area').removeClass('fa-sort-numeric-up-alt');
+      $('table tr i.area').addClass('fa-sort-numeric-down');
+      sortByAreaUp();
+    }
     refreshTable(dataList);
   });
 
     // rendezés populáció szerint
-  $('table tr i.population').click(function(){
-    deleteTr;
-    sortByPopulation();
-    refreshTable(dataList);
-  });
+    $('table tr i.population').click(function(){
+      deleteTr;
+      if ($('table tr i.population').hasClass('fa-sort-numeric-down')) {
+        $('table tr i.population').removeClass('fa-sort-numeric-down');
+        $('table tr i.population').addClass('fa-sort-numeric-up-alt');
+        sortByPopulationDown();
+      }else{
+        $('table tr i.population').removeClass('fa-sort-numeric-up-alt');
+        $('table tr i.population').addClass('fa-sort-numeric-down');
+        sortByPopulationUp();
+      }
+      refreshTable(dataList);
+    });
 
   function initializeGMap(lat, lng) {
       myLatlng = new google.maps.LatLng(lat, lng);
-
       var myOptions = {
         zoom: 5,
         zoomControl: true,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-
       map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-
       myMarker = new google.maps.Marker({
         position: myLatlng
       });
