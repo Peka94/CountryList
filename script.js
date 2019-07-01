9730639836816bd04caa0bdf27ecc8b0d6ef2156
Bence Cserfalvi <bence.cserfalvi@gmail.com>
2019-07-01 14:42:56 +0200

    Map mk1

--- script.js
+++ script.js
@@ -46,4 +46,15 @@ $(() => {
 
   }, );
 
+
+
 });
+function initMap() {
+  // The location
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map centered
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker
+  var marker = new google.maps.Marker({position: uluru, map: map});
+};
